404c0d966011b025ee6e908d2a50fccd91ac6838
Muharrem Günal <muharemgunal@gmail.com>
2021-08-16 21:25:11 +0300

    updated pizza-app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo} alt="title" />
         <div className="hiddenLinks">
           <Link to="/">Home</Link>
           <Link to="/menu">Menu</Link>
